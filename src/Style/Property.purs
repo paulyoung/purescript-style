73fa15d8975b558d1f6dea0b37daacc149036762
Paul Young <paulyoungonline@gmail.com>
2018-05-14 21:29:17 -0700

    Add color and background color, update README, add example usage as a "test" (#5)
    * Add color and background color
    
    * Update README
    
    * Add example usage as a "test"
    
    * Fix test

--- src/Style/Property.purs
+++ src/Style/Property.purs
@@ -2,23 +2,56 @@ module Style.Property where
 
 import Prelude
 
+import Color as C
 import Data.Variant (Variant, expand)
 import Style.Property.Name (Name(..))
-import Style.Property.Value (Auto, Em, Pct, Px, Value)
+import Style.Property.Value (Auto, Color, Em, Pct, Px, Value, Zero)
+import Style.Property.Value (color) as V
+import Style.Render.Value (value)
 import Type.Row (type (+))
 
 data Property = Property Name Value
 
 derive instance eqProperty :: Eq Property
-derive instance ordProperty :: Ord Property
+
+instance ordProperty :: Ord Property where
+  compare (Property n1 v1) (Property n2 v2) =
+    n1 `compare` n2 <> value v1 `compare` value v2
 
 instance showProperty :: Show Property where
   show (Property n v) = "(Property " <> show n <> " " <> show v <> ")"
 
 
+type BackgroundColorValue =
+  Variant
+    ( Color
+    + ()
+    )
+
+backgroundColor :: C.Color -> Property
+backgroundColor = backgroundColor' <<< V.color
+  where
+  backgroundColor' :: BackgroundColorValue -> Property
+  backgroundColor' = Property BackgroundColor <<< expand
+
+
+type ColorValue =
+  Variant
+    ( Color
+    + ()
+    )
+
+color :: C.Color -> Property
+color = color' <<< V.color
+  where
+  color' :: ColorValue -> Property
+  color' = Property Color <<< expand
+
+
 type FontSizeValue =
   Variant
     ( Px
+    + Zero
     + ()
     )
 
@@ -32,6 +65,7 @@ type HeightValue =
     + Em
     + Pct
     + Px
+    + Zero
     + ()
     )
 
@@ -45,6 +79,7 @@ type MarginValue =
     + Em
     + Pct
     + Px
+    + Zero
     + ()
     )
 
@@ -58,6 +93,7 @@ type PaddingValue =
     + Em
     + Pct
     + Px
+    + Zero
     + ()
     )
 
@@ -71,6 +107,7 @@ type WidthValue =
     + Em
     + Pct
     + Px
+    + Zero
     + ()
     )
 
