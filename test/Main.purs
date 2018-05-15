27e828627c7d4bd5975b1e21568899bb825ec596
Paul Young <paulyoungonline@gmail.com>
2018-05-14 21:22:13 -0700

    Add example usage as a "test"

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,23 @@
 module Test.Main where
 
-import Prelude
+import Prelude hiding (zero)
+
+import Color (black, rgb)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Style.Property (backgroundColor, color, fontSize, height, margin, padding, width)
+import Style.Property.Value (auto, em, pct, px, zero)
+import Style.Render (inline)
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log "You should add some tests."
+  log $
+inline
+  [ backgroundColor $ rgb 127 127 127
+  , color black
+  , fontSize $ 16.0 # px
+  , height $ 100.0 # pct
+  , margin zero
+  , padding $ 2.0 # em
+  , width auto
+  ]
