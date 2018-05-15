4ab14826c1c32cd8c815d143652a3f17486348e7
Paul Young <paulyoungonline@gmail.com>
2018-05-14 21:26:40 -0700

    Fix test

--- test/Main.purs
+++ test/Main.purs
@@ -11,13 +11,12 @@ import Style.Render (inline)
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log $
-inline
-  [ backgroundColor $ rgb 127 127 127
-  , color black
-  , fontSize $ 16.0 # px
-  , height $ 100.0 # pct
-  , margin zero
-  , padding $ 2.0 # em
-  , width auto
-  ]
+  log $ inline
+    [ backgroundColor $ rgb 127 127 127
+    , color black
+    , fontSize $ 16.0 # px
+    , height $ 100.0 # pct
+    , margin zero
+    , padding $ 2.0 # em
+    , width auto
+    ]
