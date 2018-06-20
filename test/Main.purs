351623f68d37ccacbe5f17567b17afb4b607bc02
Paul Young <paulyoungonline@gmail.com>
2018-06-19 22:17:04 -0700

    Add shorthand functions

--- test/Main.purs
+++ test/Main.purs
@@ -7,8 +7,8 @@ import Data.Array.NonEmpty (NonEmptyArray, fromArray)
 import Data.Maybe (Maybe, maybe)
 import Effect (Effect)
 import Effect.Console (log)
-import Style.Declaration (Declaration, backgroundColor, borderRadius, color, fontSize, fontWeight, height, margin, outlineStyle, padding, textAlign, width)
-import Style.Declaration.Value (auto, center, em, none, number, pct, px, zero)
+import Style.Declaration (Declaration, backgroundColor, borderRadius, color, fontSize, fontWeight, height, margin, outline, padding, textAlign, width)
+import Style.Declaration.Value (auto, center, currentColor, em, in_, mm, number, pct, px, rem, solid, zero)
 import Style.Render (inline)
 import Style.Ruleset (Ruleset(..))
 import Style.Ruleset as Ruleset
@@ -22,19 +22,19 @@ selectors = fromArray
   ]
 
 declarations :: Maybe (NonEmptyArray Declaration)
-declarations = fromArray
+declarations = fromArray $
   [ backgroundColor $ rgb 127 127 127
-  , borderRadius $ 8.0 # px
   , color black
   , fontSize $ 16.0 # px
   , fontWeight $ number 300.0
   , height $ 100.0 # pct
-  , margin zero
-  , outlineStyle none
-  , padding $ 2.0 # em
   , textAlign center
   , width auto
   ]
+  <> borderRadius (8.0 # px) (8.0 # px) (8.0 # px) (8.0 # px)
+  <> margin auto (8.0 # px) (50.0 # pct) zero
+  <> outline (1.0 # px) solid currentColor
+  <> padding (1.0 # em) (2.0 # in_) (3.0 # mm) (4.0 # rem)
 
 ruleset :: Maybe Ruleset
 ruleset = Ruleset <$> selectors <*> declarations
